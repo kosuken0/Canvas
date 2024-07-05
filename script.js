953c4591db141fbfb7150c2298f6c0af5220495f
Kosuken <119076494+kosuken0@users.noreply.github.com>
2024-07-05 23:07:25 +0000

    arranging

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 // console.log(ctx);
 function draw() {
+    //ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = '#DB9D5A';
     ctx.strokeStyle = '#B17F49';
     ctx.lineWidth = 10;
@@ -12,9 +13,18 @@ function draw() {
     ctx.fill();
     ctx.stroke();
 };
+function drawcont(x, y, w, h, r){
+    ctx.fillStyle = '#DB9D5A';
+    ctx.strokeStyle = '#B17F49';
+    ctx.lineWidth = 10;
+    ctx.beginPath();
+    ctx.roundRect(x, y, w, h, [r]);
+    ctx.fill();
+    ctx.stroke();
+}
 window.addEventListener('resize', function(){
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
     draw();
 });
-draw();
+drawcont(20, 20, canvas.width-40, canvas.height-40, 15)
