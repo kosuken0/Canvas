80a07d63ac259bece7972d16e87519bb93fd6c33
Kosuken <119076494+kosuken0@users.noreply.github.com>
2024-07-05 23:21:37 +0000

    omg isso cool now

--- script.js
+++ script.js
@@ -3,18 +3,19 @@ const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 // console.log(ctx);
-function drawcont(x, y, w, h, r){
-    ctx.fillStyle = '#DB9D5A';
-    ctx.strokeStyle = '#B17F49';
-    ctx.lineWidth = 10;
+function drawcont(x, y, width, height, borderw, round, bordercol, fillcol){
+    ctx.fillStyle = fillcol;
+    ctx.strokeStyle = bordercol;
+    ctx.lineWidth = borderw;
     ctx.beginPath();
-    ctx.roundRect(x, y, w, h, [r]);
+    ctx.roundRect(x, y, width, height, [round]);
     ctx.fill();
     ctx.stroke();
 }
 function draw() {
     //ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawcont(20, 20, canvas.width-40, canvas.height-40, 15)
+    drawcont(20, 20, canvas.width-40, canvas.height-40, 10, 15, '#B17F49', '#DB9D5A');
+    drawcont(35, 35, canvas.width*0.5-70, canvas.height-70, 10, 15, '#b17f49', '#db9d5a')
 };
 window.addEventListener('resize', function(){
     canvas.width = window.innerWidth;
