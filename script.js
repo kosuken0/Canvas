24be5299fe21856daea9acd12a1eff002707b90d
Kosuken <119076494+kosuken0@users.noreply.github.com>
2024-07-05 15:04:08 +0200

    hmm

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-console.log(ctx);
+// console.log(ctx);
 window.addEventListener('resize', function(){
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
