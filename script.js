62ced9131cf7b4483292ca505038b98251e7b254
Kosuken <119076494+kosuken0@users.noreply.github.com>
2024-07-06 01:10:52 +0000

    w&h to x2y2

--- script.js
+++ script.js
@@ -3,21 +3,21 @@ const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 // console.log(ctx);
-function drawcont(x, y, width, height, borderw, round, bordercol, fillcol){
+function drawcont(x, y, x2, y2, borderw, round, bordercol, fillcol){
     ctx.strokeStyle = bordercol;
     ctx.fillStyle = fillcol;
     ctx.lineWidth = borderw;
     ctx.beginPath();
-    ctx.roundRect(x, y, width, height, [round]);
+    ctx.roundRect(x, y, x2-x, y2-y, [round]);
     ctx.fill();
     ctx.stroke();
 };
 function draw() {
     //ctx.clearRect(0, 0, canvas.width, canvas.height);
-    //drawcont(x, y, width, height, borderw, round, bordercol, fillcol);
-    drawcont(20, 20, canvas.width-40, canvas.height-40, 10, 15, '#B17F49', '#DB9D5A');
-    drawcont(35, 35, canvas.width*0.5-45, canvas.height-70, 10, 15, '#b17f49', '#db9d5a');
-    drawcont(canvas.width*0.5+10, 35, canvas.width*0.5-45, canvas.height-70, 10, 15, '#b17f49', '#db9d5a');
+    //drawcont(x, y, x2, y2, borderw, round, bordercol, fillcol);
+    drawcont(20, 20, canvas.width-20, canvas.height-20, 10, 15, '#B17F49', '#DB9D5A');
+    drawcont(35, 35, canvas.width*0.5-10, canvas.height-35, 10, 15, '#b17f49', '#db9d5a');
+    drawcont(canvas.width*0.5+10, 35, canvas.width-35, canvas.height-35, 10, 15, '#b17f49', '#db9d5a');
     
 };
 window.addEventListener('resize', function(){
