3bd7a3b3e4124817fa5c412b1383d60f5c813226
Kosuken <119076494+kosuken0@users.noreply.github.com>
2024-07-06 02:06:21 +0200

    :3

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 // console.log(ctx);
 function drawcont(x, y, width, height, borderw, round, bordercol, fillcol){
-    ctx.fillStyle = fillcol;
     ctx.strokeStyle = bordercol;
+    ctx.fillStyle = fillcol;
     ctx.lineWidth = borderw;
     ctx.beginPath();
     ctx.roundRect(x, y, width, height, [round]);
@@ -16,8 +16,8 @@ function draw() {
     //ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawcont(20, 20, canvas.width-40, canvas.height-40, 10, 15, '#B17F49', '#DB9D5A');
     drawcont(35, 35, canvas.width*0.5-45, canvas.height*0.75-45, 10, 15, '#b17f49', '#db9d5a');
-    drawcont(canvas.width*0.5+10, 35, canvas.width*0.5-45, canvas.height*0.75-45, 10, 15, '#b17f49', '#db9d5a')
-    drawcont(35, canvas.height*0.75+10, canvas.width-70, canvas.height*0.25-45, 10, 15, '#b17f49', '#db9d5a')
+    drawcont(canvas.width*0.5+10, 35, canvas.width*0.5-45, canvas.height*0.75-45, 10, 15, '#b17f49', '#db9d5a');
+    drawcont(35, canvas.height*0.75+10, canvas.width-70, canvas.height*0.25-45, 10, 15, '#b17f49', '#db9d5a');
 };
 window.addEventListener('resize', function(){
     canvas.width = window.innerWidth;
